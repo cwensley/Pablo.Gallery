9f53929062b0ea45e54f40515f63b6d16939a43c
Curtis Wensley <curtis.wensley@gmail.com>
2014-06-24 23:21:55 -0700

    Remove unused packages

--- src/Pablo.Gallery/App_Start/BundleConfig.cs
+++ src/Pablo.Gallery/App_Start/BundleConfig.cs
@@ -8,10 +8,6 @@ namespace Pablo.Gallery
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-				"~/Scripts/jquery-ui-{version}.js"
-			));
-
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 				"~/Scripts/jquery.unobtrusive*",
 				"~/Scripts/jquery.validate*"
@@ -42,20 +38,7 @@ namespace Pablo.Gallery
 			bundle.Transforms.Add(new CssMinify());
 			bundles.Add(bundle);
 
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-				"~/Content/themes/base/jquery.ui.core.css",
-				"~/Content/themes/base/jquery.ui.resizable.css",
-				"~/Content/themes/base/jquery.ui.selectable.css",
-				"~/Content/themes/base/jquery.ui.accordion.css",
-				"~/Content/themes/base/jquery.ui.autocomplete.css",
-				"~/Content/themes/base/jquery.ui.button.css",
-				"~/Content/themes/base/jquery.ui.dialog.css",
-				"~/Content/themes/base/jquery.ui.slider.css",
-				"~/Content/themes/base/jquery.ui.tabs.css",
-				"~/Content/themes/base/jquery.ui.datepicker.css",
-				"~/Content/themes/base/jquery.ui.progressbar.css",
-				"~/Content/themes/base/jquery.ui.theme.css"
-			));
+			bundles.Add(new StyleBundle("~/Content/themes/base/css"));
 		}
 
 		public static void RegisterExternalBundles(BundleCollection bundles)
